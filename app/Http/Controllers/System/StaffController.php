6ebade03ece0efadc537ce4a0f4bf37a17041353
Jay Lao <cjaylao447@gmail.com>
2026-03-10 10:19:34 +0800

    🔧 update: fix namespace for User model in StaffController

--- app/Http/Controllers/System/StaffController.php
+++ app/Http/Controllers/System/StaffController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\System;
 
 use App\Http\Controllers\Controller;
 
-use App\Models\User;
+use App\Models\System\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
