c9956cf9edac0418b35afd7cce1e6a31977b45bb
Jay Lao <cjaylao447@gmail.com>
2026-03-10 09:15:49 +0800

    🔧 update: correct namespace in InventoryMovement model

--- app/Models/Inventory/InventoryMovement.php
+++ app/Models/Inventory/InventoryMovement.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Inventory;
 
+use App\Models\System\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
